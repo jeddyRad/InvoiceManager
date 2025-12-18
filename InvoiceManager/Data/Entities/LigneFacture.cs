83e7742a50368043329e05c9e4d51d6f0e49b0de
Jeddy <Jeddy@DESKTOP-FIFCP52>
2025-12-18 13:17:29 +0300

    Ajout de la gestion dynamique des lignes de factures et du calcul automatique des totaux dans FactureForm

--- InvoiceManager/Data/Entities/LigneFacture.cs
+++ InvoiceManager/Data/Entities/LigneFacture.cs
@@ -10,14 +10,15 @@ namespace InvoiceManager.Data.Entities
         public string Description { get; set; } = string.Empty;
 
         [Range(1, int.MaxValue)]
-        public int Quantite { get; set; }
+        public int Quantite { get; set; } = 1;
 
         [Range(0, double.MaxValue)]
-        public decimal PrixUnitaire { get; set; }
+        public decimal PrixUnitaire { get; set; } = 0;
 
-        public decimal TotalLigne { get; set; }
+        public decimal TotalLigne => Quantite * PrixUnitaire;
 
         public int FactureId { get; set; }
         public Facture Facture { get; set; } = null!;
     }
+
 }
